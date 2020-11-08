765e4796b4b16a473c5a1ebbdf4b2aae79b299d6
keyman500 <mar-chy@hotmail.com>
2020-11-08 00:00:42 +0000

    refracted code

---
+++ add.js
@@ -0,0 +1,51 @@
+var obj = {};
+var title = document.getElementById("title");
+var post = document.getElementById("post");
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCBEARnVWbgDONyoVneVxK-_9XMRy2jUv4",
+    authDomain: "persona-hci.firebaseapp.com",
+    databaseURL: "https://persona-hci.firebaseio.com",
+    projectId: "persona-hci",
+    storageBucket: "persona-hci.appspot.com",
+    messagingSenderId: "994643555305",
+    appId: "1:994643555305:web:97ca9a9fa8b29e7261d178",
+    measurementId: "G-F9FP8SB8Z2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+
+function send(){
+    var obj = {};
+var title = document.getElementById("title");
+var post = document.getElementById("post");
+var img = document.getElementById("img");
+    if(img.value){
+obj = {
+    "title":title.value,
+    "img":img.value,
+    "post":post.value
+}; }else{
+obj = {
+"title":title.value,
+"post": post.value
+
+}
+
+}
+console.log(img.value);
+console.log(post.value);
+var db = firebase.firestore();
+db.collection("posts").doc().set(obj)
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error writing document: ", error);
+});
+
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCBEARnVWbgDONyoVneVxK-_9XMRy2jUv4",
+    authDomain: "persona-hci.firebaseapp.com",
+    databaseURL: "https://persona-hci.firebaseio.com",
+    projectId: "persona-hci",
+    storageBucket: "persona-hci.appspot.com",
+    messagingSenderId: "994643555305",
+    appId: "1:994643555305:web:97ca9a9fa8b29e7261d178",
+    measurementId: "G-F9FP8SB8Z2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+var db = firebase.firestore();
+var posts = document.getElementById("posts");
+
+db.collection("posts").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+    if(doc.data().img){
+     posts.innerHTML+=` <div class="card">
+      <h2>${doc.data().title}</h2>
+      <img src="${doc.data.img}" id="logo"/>
+      <p>${doc.data().post}</p>
+    </div>`}
+    else{
+    posts.innerHTML+=` <div class="card">
+      <h2>${doc.data().title}</h2> 
+      <p>${doc.data().post}</p>
+    </div>`
+    }
+        console.log(doc.id, " => ", doc.data());
+    });
+});
